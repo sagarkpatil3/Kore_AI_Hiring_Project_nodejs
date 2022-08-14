9e9a0a7f797f8e105c26058cec4e22fdceba5a78
vinayak677 <95090050+vinayak677@users.noreply.github.com>
2022-08-14 19:03:18 +0530

    port number changed

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 const milkOrder = require(__root + './routes/milkOrder');
 app.use('/milkOrder', milkOrder);
 
-const port = '8000';
+const port = '8003';
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`)
 })
